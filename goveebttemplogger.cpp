9aaee3b4ec54322012e268946df3e75eecd12cd0
William C Bonner <wimbonner@hotmail.com>
2025-04-22 14:34:42 -0700

    fix console output

--- goveebttemplogger.cpp
+++ goveebttemplogger.cpp
@@ -1013,7 +1013,7 @@ void GeneratePersistenceFile(std::map<bdaddr_t, time_t>& PersistenceData, std::m
 		if (ConsoleVerbosity > 1)
 			for (auto const& [TheAddress, TheType] : ThermometerTypes)
 				if (auto search = PersistenceData.find(TheAddress); search != PersistenceData.end())
-					std::cout << "[-------------------] [" << ba2string(TheAddress) << "\t" << ThermometerType2String(TheType) << "\t" << timeToISO8601(search->second) << std::endl;
+					std::cout << "[-------------------] [" << ba2string(TheAddress) << "] " << ThermometerType2String(TheType) << " " << timeToISO8601(search->second) << std::endl;
 		// If PersistenceData has updated information, write new data to file
 		std::filesystem::path filename(LogDirectory / PersistenceFileName);
 		time_t MostRecentDownload(0);
